5ead3422e702b3eb3f9b2443dc281e5f4a101fbf
gthio <gunawanthio@outlook.com>
2016-04-19 23:36:25 +0800

    no message

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import {bootstrap} from 'angular2/platform/browser';
 import {provide} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
-
 import {WINDOW, WINDOW_PROVIDERS} from './app/services/window-service';
 import {SeedApp} from './app/seed-app';
 
